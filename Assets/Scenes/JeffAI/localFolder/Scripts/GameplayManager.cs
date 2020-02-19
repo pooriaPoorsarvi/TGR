c7bb2253df55a48e87164cf1b4737e62db0bb7a9
Jeffrey <jeffreynassyrov8@gmail.com>
2020-02-18 20:27:49 -0500

    implemented pause menu

--- Assets/Scenes/JeffAI/localFolder/Scripts/GameplayManager.cs
+++ Assets/Scenes/JeffAI/localFolder/Scripts/GameplayManager.cs
@@ -12,9 +12,26 @@ public class GameplayManager : MonoBehaviour
     // ui element that represents the game over screen
     public static GameObject gameOverScreen;
 
+    public GameObject pauseMenu;
+
     // restart the game after player has lost
     public void Retry(){
         Application.LoadLevel(Application.loadedLevel);
+    }
+    
+    public void PauseGame(){
+    	Debug.Log("Game paused");
+    	pauseMenu.active = true;
+    }
+
+    // continue game
+    public void ContinueGame(){
+        pauseMenu.active = false;
+    }
+
+    // quit game
+    public void QuitGame(){
+        Application.Quit();
     }
 
     void Start(){
