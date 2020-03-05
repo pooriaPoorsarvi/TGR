a3b33d86bf1eb7be1fb5eadf69c12c4f904ca8e3
Jeffrey <jeffreynassyrov8@gmail.com>
2020-03-05 01:06:22 -0500

    minor fix

--- Assets/Scenes/Max/Scripts/MainMenu.cs
+++ Assets/Scenes/Max/Scripts/MainMenu.cs
@@ -56,12 +56,20 @@ public class MainMenu : MonoBehaviour
     
     void ProcessMovement(Vector2 currMovement)
     {
-        move = new Vector3(currMovement.x * speedX, currMovement.y * speedY, 0) * Time.deltaTime;
+        move = new Vector3(currMovement.x * speedX, currMovement.y * speedY, 0);
     }
 
     void Update()
     {
         mousePointer.transform.position += move;
+
+        if(Input.GetKeyDown(KeyCode.W)){
+        	mousePointer.transform.position += new Vector3(0f, 20f, 0f);
+        }
+
+        if(Input.GetKeyDown(KeyCode.S)){
+        	mousePointer.transform.position += new Vector3(0f, -20f, 0f);
+        }
 
         for(int i = 0; i < buttonActions.Length; i++){
         	if(Overlaps(buttonActions[i].button, mousePointer)){
