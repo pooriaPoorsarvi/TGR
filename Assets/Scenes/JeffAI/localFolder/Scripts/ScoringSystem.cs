4ad89904185623c53eb8043b07c496cf27db77bd
Jeffrey <jeffreynassyrov8@gmail.com>
2020-03-09 03:25:59 -0500

    implemented score calculation and saving

--- Assets/Scenes/JeffAI/localFolder/Scripts/ScoringSystem.cs
+++ Assets/Scenes/JeffAI/localFolder/Scripts/ScoringSystem.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
 
 public class ScoringSystem : MonoBehaviour
 {
@@ -8,15 +11,96 @@ public class ScoringSystem : MonoBehaviour
     // index of the current level
     public int levelIndex;
 
-    // Calculate and return score for the level
-    private int CalculateScore(){
-        return 0; // for simplicity
+    public GameObject limbsLostText;
+    public GameObject killsText;
+    public GameObject timeElapsedText;
+    public GameObject objectsUsedText;
+    public GameObject timesFallenText;
+
+    private static int limbsLost;
+    private static int kills;
+    private static int objectsUsed;
+    private static int timesFallen;
+
+    public int limbsUpperLimit;
+    public int killsUpperLimit;
+    public int objectsUsedUpperLimit;
+    public int timesFallenUpperLimit;
+
+    public GameObject finalGrade;
+
+    public static void LimbLost(){
+        limbsLost++;
     }
 
-    // added score for the current level
-    private void SaveScore(){
-  
-        int curScore = CalculateScore();
+    public static void KillMade(){
+        kills++;
+    }
+
+    public static void ObjectWasUsed(){
+        objectsUsed++;
+    }
+
+    public static void FellDown(){
+        timesFallen++;
+    }
+
+    private String AssignLetterGrade(float result){
+        if(result >= 4.5f){
+            return "A+";
+        }
+        else if(result >= 4.2f){
+            return "A";
+        }
+        else if(result >= 4f){
+            return "B+";
+        }
+        else if(result >= 3.7){
+            return "B";
+        }
+        else if(result >= 3.2){
+            return "B";
+        }
+        else if(result >= 3){
+            return "B-";
+        }
+        else if(result >= 2.5){
+            return "C+";
+        }
+        else if(result >= 2f){
+            return "C";
+        }
+        else if(result >= 1.5f){
+            return "C-";
+        }
+        else{
+            return "F";
+        }                                                
+    }
+
+    public void UpdateScore(int timeElapsed, int timeUpperLimit){
+        limbsLostText.GetComponent<TextMeshProUGUI>().text = limbsLost + "/" + limbsUpperLimit;
+        killsText.GetComponent<TextMeshProUGUI>().text = kills + "/" + killsUpperLimit;
+        timeElapsedText.GetComponent<TextMeshProUGUI>().text = timeElapsed + "/" + timeUpperLimit;
+        objectsUsedText.GetComponent<TextMeshProUGUI>().text = objectsUsed + "/" + objectsUsedUpperLimit;
+        timesFallenText.GetComponent<TextMeshProUGUI>().text = timesFallen + "/" + timesFallenUpperLimit;
+
+        float result = CalculateScore(timeElapsed, timeUpperLimit);
+        String letterGrade = AssignLetterGrade(result);
+        finalGrade.GetComponent<TextMeshProUGUI>().text = letterGrade;
+    }
+
+
+
+    // Calculate and return score for the level
+    private float CalculateScore(int timeElapsed, int timeUpperLimit){
+        return (limbsLost / (float)limbsUpperLimit + kills / (float)killsUpperLimit - 
+             timeUpperLimit / (float)timeElapsed + objectsUsed / (float) objectsUsedUpperLimit + 
+            timesFallen / (float)timesFallenUpperLimit);
+    }
+
+    // Uses PlayerPrefs to save data
+    private void SaveScore(int curScore){
         PlayerPrefs.SetInt("level" + levelIndex, curScore);
 	    PlayerPrefs.Save();
 
@@ -24,7 +108,6 @@ public class ScoringSystem : MonoBehaviour
 
     void Start(){
 
-        //TimerSystem.TimerEnded += SaveScore;
 
     }
 
