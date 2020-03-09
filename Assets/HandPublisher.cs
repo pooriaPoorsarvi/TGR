ecfd7635d8365fe8401e5eebf8be2c4c1a08c007
poori <pooria.poorsarvi.tehrani@mail.utoronto.ca>
2020-03-08 22:41:43 -0400

    Hand Event Added

---
+++ Assets/HandPublisher.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandPublisher : MonoBehaviour
+{
+    public Graber grabberL;
+    public Graber grabberR;
+
+    public bool isBeingHeld(GameObject gameObject)
+    {
+        return grabberL.getHoldingGameObject().Equals(gameObject) || grabberR.getHoldingGameObject().Equals(gameObject);
+    }
+}
