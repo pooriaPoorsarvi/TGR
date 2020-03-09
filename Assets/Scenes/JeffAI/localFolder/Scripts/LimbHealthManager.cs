da90ba007db4ec4ac37badd406aa8c1e063935de
Jeffrey <jeffreynassyrov8@gmail.com>
2020-03-08 20:31:23 -0500

    updated limb health manager

--- Assets/Scenes/JeffAI/localFolder/Scripts/LimbHealthManager.cs
+++ Assets/Scenes/JeffAI/localFolder/Scripts/LimbHealthManager.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
 
 public class LimbHealthManager : MonoBehaviour
 {
@@ -12,9 +14,57 @@ public class LimbHealthManager : MonoBehaviour
     	Rleg // 3
     }
 
-    public void LoseLimbAtIndex(int i){
+    public LimbStats[] stats;
 
+    public int subAmount = 100;
+
+    public Color deadColor, aliveColor;
+
+    void Awake(){
+        for(int i = 0; i < stats.Length; i++){
+	        if(stats[i].limbHealth <= 0){
+	        	stats[i].limbTick.color = deadColor;
+	        }
+	        else{
+	        	stats[i].limbTick.color = aliveColor;
+	        }
+        }    	
+    }
+
+    private LimbStats findLimbsStatsWithIndex(int index){
+        for(int i = 0; i < stats.Length; i++){
+        	if(stats[i].limbIndex == index){
+        		return stats[i];
+        	}
+        }
+        throw new Exception("limb data not provided");
+    }
+
+    private void UpdateUI(LimbStats limbStats){
+        if(limbStats.limbHealth <= 0){
+        	limbStats.limbTick.color = deadColor;
+        }
+        else{
+        	limbStats.limbTick.color = aliveColor;
+        }
+    }
+
+    public void SubLimbHealthAtIndex(int i){
+        LimbStats limbStats = findLimbsStatsWithIndex(i);
+    	limbStats.limbHealth -= subAmount;
+    	if(limbStats.limbHealth < 0){
+    		limbStats.limbHealth = 0;
+    	}
+    	UpdateUI(limbStats);
     }
 
 
+}
+
+[System.Serializable]
+public struct LimbStats{
+    public int limbIndex;
+    public int limbHealth;
+    public int healthInitial;
+    public Image limbTick;
 }
